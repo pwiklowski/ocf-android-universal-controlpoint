dce71f04e095af8cacd45509ddb0b178e0bb86b0
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-10-29 16:32:45 +0200

    Enable full screen mode

--- app/src/main/java/com/wiklosoft/ocf/controlpoint/MainActivity.java
+++ app/src/main/java/com/wiklosoft/ocf/controlpoint/MainActivity.java
@@ -2,6 +2,7 @@ package com.wiklosoft.ocf.controlpoint;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 
 public class MainActivity extends FragmentActivity {
@@ -15,6 +16,19 @@ public class MainActivity extends FragmentActivity {
                 .replace(R.id.container, new DeviceListFragment())
                 .commit();
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 
 }
